d43a1723a0ace60ba6858e94206f0578eeff7086
jdemovic <jdemovic@gmail.com>
2022-05-19 13:42:33 +0200

    19052022V2

--- new_channel_names.sql
+++ new_channel_names.sql
@@ -1,142 +1,168 @@
-Jednotka
-Dvojka
-RTVS :24
-Markiza
-Markíza International 
-Dajto
-JoJ
-JoJ Plus
-JoJ Cinema
-JoJ Family
-Wau
-Doma
-TA3
-TV Lux
-ČT1
-ČT2
-ČT3
-ČT24
-ČT :D HD / ČT art 
-Prima 
-Prima Star
-Prima Show
-Prima COOL
-Prima ZOOM
-Prima Love
-Prima MAX
-Prima Krimi
-CNN Prima News
-Nova
-Nova Gold
-Nova Cinema
-Nova Action
-Nova Fun
-TV Barrandov
-Krimi Barrandov
-Nova Lady
-Kino Barrandov
-Nova International
-TV Paprika
-TV Seznam
-TV Noe
+13 Ulica
+360TuneBox
+AleKino+
 AMC
-HBO
-HBO2
-HBO3
-Cinemax
-Cinemax 2
-CS Horror
-Filmbox
-Filmbox Extra
-Filmbox Premium
-Filmbox Family
-FilmBox Stars
-CS Film
-Film+
-Film Europe +
-Film Europe
+Animal Planet
+Animal Planet
+Antyradio
+Arcadia world
+Arena Sport 1
+Arena Sport 2
+Auto Motor und Sport Channel
+AXN
 AXN
 AXN Black
 AXN White
-Film PRO
-Paramount Netvork
-True Amateurs
+AXN White
 Babes TV
-Erox
-Eroxxx 
-Dorcel TV
-Dorcel XXX 
-Extasy 4K
-Private TV
+Baby TV
+BBC Earth
+BBC WORLD NEWS
 Brazzers
-Playboy TV
-Leo Gold
-Leo TV
-Reality Kings
-Red Light 
-Extasy 
-History Channel
+Bundesliga 1
+Bundesliga 2
+Bundesliga 3
+Bundesliga 4
+Bundesliga 5
+Canal+ Dokument
+Canal+ Family
+Canal+ Film
+Canal+ Premium
+Canal+ Seriale
+Canal+ Sport
+Canal+ Sport 2
+Cartoon Network
+Cartoon Network
+Cinemax
+Cinemax
+Cinemax 2
+Cinemax 2
+CNN
+CNN Prima News
+Comedy Central
+Crime+Investigation Polsat
+CS Film
+CS Horror
 CS Mystery
-Viasat Explore
-Viasat History
-Viasat Nature 
+CT Decko
+ČT :D HD / ČT art
+ČT Sport
+ČT1
+ČT2
+ČT24
+ČT3
+Dajto
+Das Erste
+Discovery
+Discovery Channel
+Discovery Science
 Discovery Science
 Discovery Turbo  Xtra
-Discovery
-Spektrum
-Investigation Discovery
-Sat crime invest
-National Geographic
-Nat Geo WILD
-Animal Planet
-TLC
-NASA TV
-DocuBox 
-NASA TV UHD
-Travel XP
-Love Nature
-Travel Channel
-TV Natura 
-CNN
-Arcadia world
-Spektrum Home
-JimJam
-Nickelodeon
-Nick Jr.
-Nicktoons
-Cartoon Network
-Minimax
-LALA TV
-Rik
-Jojko
-Baby TV
 Disney Channel
 Disney Junior
-CT Decko
-Niceklodeon Ukraine
-ČT Sport 
+Disney Junior
+DocuBox
+Doma
+Dorcel TV
+Dorcel XXX
+Dvojka
+Eleven Sports 1
+Eleven Sports 2
+Eleven Sports 3
+Eleven Sports 4
+Erox
+Eroxxx
+ESKA TV
+Eurosport 1
 Eurosport 1
 Eurosport 2
-Sport1 
-Sport2 
-Sport 5
+Eurosport 2
+Extasy
+Extasy 4K
+Extreme Sports Channel
+Fastnfunbox
+Fightbox
+Film Europe
+Film Europe +
+Film PRO
+Film+
+Filmbox
+Filmbox Extra
+Filmbox Family
+Filmbox Premium
+FilmBox Stars
+Fishinghunting
+Fokus TV
+FOX
+FOX Comedy
+France 24
+Goldstar TV
+Golf Channel
+HBO
+HBO
+HBO 2
+HBO 3
+HBO2
+HBO3
+HGTV
+History
+History Channel
+Chucktv
+iConcerts
+Investigation Discovery
+Jednotka
+JimJam
+JoJ
+JoJ Cinema
+JoJ Family
+JoJ Plus
+JoJ Šport
+Jojko
+Kino Barrandov
+Kino Polska
+Kino TV
+Krimi Barrandov
+KSW
+Kuchnia+
+LALA TV
+Leo Gold
+Leo TV
+Love Nature
+Markiza
+Markíza International
+Mezzo HD
+Minimax
+MiniMini+
+MTV
+MTV  Yo !
+MTV 00s
+MTV Hits
+NASA TV
+NASA TV UHD
+Nat Geo People
+Nat Geo WILD
+National Geographic
+National Geographic Channel
+National Geographic Wild
+Niceklodeon Ukraine
+NICK JR
+Nick Jr.
+Nickelodeon
+Nicktoons
+Nova
+Nova Action
+Nova Cinema
+Nova Fun
+Nova Gold
+Nova International
+Nova Lady
 Nova Sport 1
 Nova Sport 2
 Nova Šport 3
-Nova Šport 4 
-Golf Channel
-Auto Motor und Sport Channel
-Arena Sport 1 
-Arena Sport 2
-Chucktv 
-Premier Sports 1 
-Premier Sports 2
-Fishinghunting
-Fightbox
-Fastnfunbox
-O2TV Sport
+Nova Šport 4
+nSport+
 O2TV Fotbal
-O2TV Tenis
+O2TV Sport
 O2TV Sport 1
 O2TV Sport 2
 O2TV Sport 3
@@ -145,133 +171,107 @@ O2TV Sport 5
 O2TV Sport 6
 O2TV Sport 7
 O2TV Sport 8
-JoJ Šport
-RTVS Šport
-Šlágr 2
-Šlágr TV
-Retro Music TV
-RELAX
-Rebel
-Óčko Star 
+O2TV Tenis
+Ocko Black
 Óčko
 Óčko Expres
-MTV
-TuneBox 
-MTV Hits
-Mezzo HD
-Šlágr Premium 
-MTV  Yo !
-Goldstar TV
-360TuneBox
-Ocko Black
-iConcerts
-MTV 00s
-BBC WORLD NEWS
-TV5 MONDE
-France 24
+Óčko Star
 ORF 1
 ORF 2
-Das Erste
-ZDF
-TVP 1
-TVP 2
-TVN
-TVN 7
+Paramount Netvork
+Planete+
+Playboy TV
+Polo TV
 Polsat
-TV 4
 Polsat 2
-TV Puls
-TVP Polonia
-TV 6
-TTV
-Puls 2
-TVP Info
-TVN 24
-TVN 24 BiS
-Polsat News
-Canal+ Premium
-Canal+ Film
-Canal+ Family
-HBO
-HBO 2
-HBO 3
-TVN Fabuła
-AXN
-Polsat Film
-Kino Polska
-TVP Seriale
-Cinemax
-Cinemax 2
-FOX
-Canal+ Seriale
-Crime+Investigation Polsat
-SCI FI
-TNT
-HGTV
-AXN White
-AleKino+
-Kuchnia+
-Super Polsat
-13 Ulica
-Comedy Central
-Kino TV
-FOX Comedy
-Polsat Play
 Polsat Cafe
-Canal+ Sport
-Canal+ Sport 2
-Eurosport 1
-Eurosport 2
+Polsat Doku
+Polsat Film
+Polsat News
+Polsat Play
 Polsat Sport
-TVP Sport
-nSport+
-Eleven Sports 1
-Eleven Sports 2
 Polsat Sport Extra
+Polsat Sport Fight
 Polsat Sport News
-Eleven Sports 3
-Eleven Sports 4
-Bundesliga 1
-Bundesliga 2
-Bundesliga 3
-Bundesliga 4
-Bundesliga 5
 Polsat Sport Premium 1
 Polsat Sport Premium 2
 Polsat Sport Premium 3
 Polsat Sport Premium 4
 Polsat Sport Premium 5
 Polsat Sport Premium 6
-Polsat Sport Fight
-KSW
-Extreme Sports Channel
-Discovery Channel
-National Geographic Channel
-National Geographic Wild
-TVN Style
-TVN Turbo
-Fokus TV
-Planete+
-Animal Planet
-Canal+ Dokument
-BBC Earth
-Discovery Science
-TLC
-Nat Geo People
-Polsat Doku
-History
-Disney Junior
-NICK JR
-Cartoon Network
-MiniMini+
-TVP ABC
-Antyradio
 Polskie Radio Program 1
 Polskie Radio Program 2
 Polskie Radio Program 3
+Premier Sports 1
+Premier Sports 2
+Prima
+Prima COOL
+Prima Krimi
+Prima Love
+Prima MAX
+Prima Show
+Prima Star
+Prima ZOOM
+Private TV
+Puls 2
 Radio Zet
+Reality Kings
+Rebel
+Red Light
+RELAX
+Retro Music TV
+Rik
+RMF Classic
 RMF FM
 RMF Maxxx
-RMF Classic
-ESKA TV
-Polo TV
+RTVS :24
+RTVS Šport
+Sat crime invest
+SCI FI
+Spektrum
+Spektrum Home
+Sport 5
+Sport1
+Sport2
+Super Polsat
+Šlágr 2
+Šlágr Premium
+Šlágr TV
+TA3
+TLC
+TLC
+TNT
+Travel Channel
+Travel XP
+True Amateurs
+TTV
+TuneBox
+TV 4
+TV 6
+TV Barrandov
+TV Lux
+TV Natura
+TV Noe
+TV Paprika
+TV Puls
+TV Seznam
+TV5 MONDE
+TVN
+TVN 24
+TVN 24 BiS
+TVN 7
+TVN Fabuła
+TVN Style
+TVN Turbo
+TVP 1
+TVP 2
+TVP ABC
+TVP Info
+TVP Polonia
+TVP Seriale
+TVP Sport
+Viasat Explore
+Viasat History
+Viasat Nature
+Wau
+ZDF
